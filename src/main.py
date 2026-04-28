5f96f56d92419b0554b45e4c01a6bacfef9a2839
farzadoxo <farzadebrahimi.oxo@gmail.com>
2026-04-28 20:34:14 +0330

    - auto run setup added (with .env support)

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import os
+import dotenv
+
+# load ip and port from .env 
+dotenv.load_dotenv()
+HOST = os.getenv('IP')
+PORT = os.getenv('PORT')
+
+# command attribute
+apps = ['music']
+BASE_COMMAND = 'python3 manage.py'
+
+
+def main():
+    for app in apps:
+        os.system(f'{BASE_COMMAND} makemigrations {app}')
+    
+    os.system(f'{BASE_COMMAND} migrate')
+
+    os.system(f'{BASE_COMMAND} runserver {HOST}:{PORT}')
+
+
+
+if __name__ == "__main__":
+    main()
