aab222abde794db8b70ade06da7e5ce46b921bff
farzadoxo <farzadebrahimi.oxo@gmail.com>
2026-05-19 10:48:31 +0330

    - artist lenght increased

--- src/music/models.py
+++ src/music/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class Music(models.Model):
     fileName = models.CharField(max_length=50,null=False)
     title = models.CharField(max_length=50,null=True)
-    artist = models.CharField(max_length=30,null=True)
+    artist = models.CharField(max_length=50,null=True)
     isSingleTrack = models.BooleanField(null=True,default=None)
-    album = models.CharField(max_length=30,null=True)
+    album = models.CharField(max_length=50,null=True)
     hasCover = models.BooleanField(default=True,null=True)
     coverFormat = models.CharField(max_length=5,null=True)
     url = models.CharField(max_length=10,unique=True)
